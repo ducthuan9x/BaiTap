aff140a1c4b5dd3b8b1111085bfe317242f78245
unknown <you@example.com>
2022-04-14 13:50:02 +0700

    lan2

--- src/BaiTapNA/Main.java
+++ src/BaiTapNA/Main.java
@@ -3,21 +3,21 @@ package BaiTapNA;
 public class Main {
     public static void main(String[] args) {
         QuanLyXeMay qlxm = new QuanLyXeMay();
-        XeMay xm1 = new XeMay("Honda", "Den", "a", 20, 150);
-        XeMay xm2 = new XeMay("Honda", "Xanh", "b", 80, 150);
-        XeMay xm3 = new XeMay("Honda", "Do", "c", 60, 150);
+        XeMay xm1 = new XeMay("Yamaha", "vang", "sirius", 20, 150);
+        XeMay xm2 = new XeMay("Honda", "Xanh", "ways", 80, 150);
+        XeMay xm3 = new XeMay("Honda", "Do", "sh", 60, 150);
 
         qlxm.them(xm1);
         qlxm.them(xm2);
         qlxm.them(xm3);
         qlxm.in();
         qlxm.sapXep();
-        System.out.println(qlxm.timKiem("a"));
+        System.out.println(qlxm.timKiem("sh"));
 
-        XeMay xm4 = new XeMay("Honda", "Trang", "d", 70, 200);
-        qlxm.sua("c", xm4);
+        XeMay xm4 = new XeMay("Honda", "Trang", "@", 70, 200);
+        qlxm.sua("sirius", xm4);
         qlxm.in();
-        qlxm.xoa("b");
+        qlxm.xoa("ways");
         qlxm.in();
 
     }
